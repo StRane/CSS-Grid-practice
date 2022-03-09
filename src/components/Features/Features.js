4e01876b312317e63dbb79839931b0b08dfa2170
StRane <73998688+StRane@users.noreply.github.com>
2022-03-09 10:03:30 +0700

    Story section - final
    Design 2 elements with global sass sheet and grid design for picture elements

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -6,7 +6,7 @@ const Features = () => {
     <section className={styles.features}>
       <div className={styles.feature}>
         <Icons name="global" className={styles.icon} />
-        <h4>Worlds best luxury homes</h4>
+        <h4 className="dark">Worlds best luxury homes</h4>
         <p>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
           distinctio necessitatibus pariatur voluptatibus.
@@ -14,7 +14,7 @@ const Features = () => {
       </div>
       <div className={styles.feature}>
         <Icons name="trophy" className={styles.icon} />
-        <h4>Only the best properties</h4>
+        <h4 className="dark">Only the best properties</h4>
         <p>
           Voluptatum mollitia quae. Vero ipsum sapiente molestias accusamus
           rerum sed a eligendi aut quia.
@@ -22,7 +22,7 @@ const Features = () => {
       </div>
       <div className={styles.feature}>
         <Icons name="map" className={styles.icon} />
-        <h4 className={styles.h4}>All homes in top locations</h4>
+        <h4 className="dark">All homes in top locations</h4>
         <p>
           Tenetur distinctio necessitatibus pariatur voluptatibus quidem
           consequatur harum.
@@ -30,7 +30,7 @@ const Features = () => {
       </div>
       <div className={styles.feature}>
         <Icons name="key" className={styles.icon} />
-        <h4>New home in one week</h4>
+        <h4 className="dark">New home in one week</h4>
         <p>
           Vero ipsum sapiente molestias accusamus rerum. Lorem, ipsum dolor sit
           amet consectetur adipisicing elit.
@@ -38,7 +38,7 @@ const Features = () => {
       </div>
       <div className={styles.feature}>
         <Icons name="presentation" className={styles.icon} />
-        <h4>Top 1% realtors</h4>
+        <h4 className="dark">Top 1% realtors</h4>
         <p>
           Quidem consequatur harum, voluptatum mollitia quae. Tenetur distinctio
           necessitatibus pariatur voluptatibus.
@@ -46,7 +46,7 @@ const Features = () => {
       </div>
       <div className={styles.feature}>
         <Icons name="lock" className={styles.icon} />
-        <h4>Secure payments on nexter</h4>
+        <h4 className="dark">Secure payments on nexter</h4>
         <p>
           Pariatur voluptatibus quidem consequatur harum, voluptatum mollitia
           quae.
--- src/components/Story/Story.js
+++ src/components/Story/Story.js
@@ -1,10 +1,27 @@
 import styles from "./Story.module.scss";
 
+import img1 from "../../img/story-1.jpeg";
+import img2 from "../../img/story-2.jpeg";
+
 const Story = () => {
   return (
     <>
-      <div className={styles.pictures}>Story pictures</div>
-      <div className={styles.content}>Story content</div>
+      <div className={styles.pictures}>
+        <img src={img1} alt="Couple with new house" className={styles.img1} />
+        <img src={img2} alt="New house" className={styles.img2} />
+      </div>
+      <div className={styles.content}>
+        <h3 className={`mb-sm`}>Happy Customers</h3>
+        <h2 className={`dark mb-md`}>
+          &ldquo;The best decision of our lives&rdquo;
+        </h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+          distinctio necessitatibus pariatur voluptatibus. Quidem consequatur
+          harum volupta!
+        </p>
+        <button>Find your own home</button>
+      </div>
     </>
   );
 };
